068b17aae674aebce01d8727d92c41634361a24d
52917 <52917@etu.he2b.be>
2021-05-06 13:58:41 +0200

    exo 3 Pokemon, analyse

--- src/Pokemonsssss/Combat.java
+++ src/Pokemonsssss/Combat.java
@@ -3,4 +3,17 @@ package Pokemonsssss;
 public class Combat {
     Dresseur you;
     Dresseur him;
+
+    public Combat(Dresseur you, Dresseur him) {
+        this.you = you;
+        this.him = him;
+    }
+
+    public Dresseur getYou() {
+        return you;
+    }
+
+    public Dresseur getHim() {
+        return him;
+    }
 }
